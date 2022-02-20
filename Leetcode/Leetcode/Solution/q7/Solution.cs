21a3b6d288c95d209f9a45f0d2c450fd18bc0936
Vik1ang <vik1ang@outlook.com>
2022-02-20 16:54:14 +0800

    q7

---
+++ Leetcode/Leetcode/Solution/q7/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode.Solution.q7
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            var res = 0;
+            const int max = int.MaxValue / 10;
+            const int min = int.MinValue / 10;
+
+            while (x != 0)
+            {
+                int temp = x % 10;
+                if (res > max || (res == max && temp > int.MaxValue % 10))
+                {
+                    return 0;
+                }
+                if (res < min || (res == min && temp < int.MinValue % 10))
+                {
+                    return 0;
+                }
+
+                res = res * 10 + temp;
+                x /= 10;
+            }
+
+            return res;
+        }
+    }
+}
